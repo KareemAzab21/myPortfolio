70d5eb7ade19da2958ce815e68cf7ef357872488
KareemAzab21 <kaazab35@gmail.com>
2023-09-28 11:47:59 +0300

    tttt

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -6,7 +6,7 @@ import navIcon2 from '../assets/img/nav-icon2.svg';
 import navIcon3 from '../assets/img/nav-icon3.svg';
 import { HashLink } from 'react-router-hash-link';
 import {
-  HashRouter as Router
+  BrowserRouter as Router
 } from "react-router-dom";
 
 export const NavBar = () => {
@@ -36,9 +36,6 @@ export const NavBar = () => {
     <Router >
       <Navbar expand="md" className={scrolled ? "scrolled" : ""}>
         <Container>
-          {/* <Navbar.Brand href="/">
-            <img src={logo} alt="Logo" />
-          </Navbar.Brand> */}
           <Navbar.Toggle aria-controls="basic-navbar-nav">
             <span className="navbar-toggler-icon"></span>
           </Navbar.Toggle>
